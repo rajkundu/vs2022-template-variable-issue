186f5fca32d833f207d257c29e306debb6e6c5c2
Raj Kundu <rk274@duke.edu>
2023-02-22 11:52:59 -0500

    minimal reproducible example

---
+++ test.cc
@@ -0,0 +1,4 @@
+#include "test.h"
+
+template<>
+const std::string TestClass::s<float> = std::string();
---
+++ test.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+class TestClass {
+	public:
+		template <class T>
+		static const std::string s;
+};
